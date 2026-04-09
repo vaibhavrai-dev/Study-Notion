fdb81651fdd1d328adb75b509ad147c141201d75
vaibhavrai-dev <0221cs198@niet.co.in>
2026-04-09 19:09:19 +0530

    disable eslint strict mode

--- src/services/operations/pageAndComponentData.js
+++ src/services/operations/pageAndComponentData.js
@@ -1,27 +1,28 @@
-import React from 'react'
-import {toast} from "react-hot-toast"
-import { apiConnector } from '../apiconnector';
-import { catalogData } from '../apis';
+import { toast } from "react-hot-toast"
+import { apiConnector } from "../apiconnector"
+import { catalogData } from "../apis"
 
-export const getCatalogaPageData = async(categoryId) => {
-  const toastId = toast.loading("Loading...");
-  let result = [];
-  try{
-        const response = await apiConnector("POST", catalogData.CATALOGPAGEDATA_API, 
-        {categoryId: categoryId,});
+export const getCatalogaPageData = async (categoryId) => {
+  const toastId = toast.loading("Loading...")
+  let result = []
 
-        if(!response?.data?.success)
-            throw new Error("Could not Fetch Category page data");
+  try {
+    const response = await apiConnector(
+      "POST",
+      catalogData.CATALOGPAGEDATA_API,
+      { categoryId }
+    )
 
-         result = response?.data;
+    if (!response?.data?.success)
+      throw new Error("Could not Fetch Category page data")
 
+    result = response?.data
+  } catch (error) {
+    console.log("CATALOG PAGE DATA API ERROR....", error)
+    toast.error(error.message)
+    result = error.response?.data
   }
-  catch(error) {
-    console.log("CATALOG PAGE DATA API ERROR....", error);
-    toast.error(error.message);
-    result = error.response?.data;
-  }
-  toast.dismiss(toastId);
-  return result;
+
+  toast.dismiss(toastId)
+  return result
 }
-
